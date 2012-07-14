ee2f49a156118d699c392234a6a0f0a2c2a3c26e
Rob Thomas <xrobau@gmail.com>
2012-07-14 18:12:12 +1000

    Starting to look reasonable now.

--- ajax.php
+++ ajax.php
@@ -1,10 +1,15 @@
 <?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
 
 /* Load the settings from wp-config.php */
 /* Note that just including the file adds about 100msec. So we'll parse it manually */
 
 $dbhost = $dbname = $dbuser = $dbpw = 'undefined';
 $all = file("../wp-config.php");
+
+if (!isset($_REQUEST['action'])) { $_REQUEST['action'] = 'list'; }
 
 foreach($all as $line_num => $line) {
     if (preg_match("/define.'DB_NAME', '(.+)'/", $line, $m)) { $dbname = $m[1]; }
@@ -15,14 +20,24 @@ foreach($all as $line_num => $line) {
 
 $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpw, array( PDO::ATTR_PERSISTENT => true));
 
+if ($_REQUEST['action'] === 'list') {
+    $sql = 'select distinct(substr(derbyname, 1,1)) as initial from rollerderby_players order by initial';
+    $query = $dbh->query($sql);
+    $data = $query->fetchAll(PDO::FETCH_COLUMN, 0);
+    print json_encode($data);
+    exit;
+}
+
 /* We should have the stuff typed in _REQUEST['term']. */
-if(isset($_REQUEST['term'])) {
-      $r = $_REQUEST['term'];
-      if (preg_match('/>|</', $r)) {
-          print json_encode(array('Error. Invalid Chars'));
-          exit;
-      }
-      print json_encode(array("I have $r"));
-  }
+if ($_REQUEST['action'] === 'search') {
+    if(isset($_REQUEST['term'])) {
+        $r = $_REQUEST['term'];
+        if (preg_match('/>|</', $r)) {
+            print json_encode(array('Error. Invalid Chars'));
+            exit;
+        }
+        print json_encode(array("I have $r"));
+    }
+}
   
 ?>
--- index.php
+++ index.php
@@ -34,18 +34,22 @@
         table { width: 100%; border-collapse: collapse;}
         .ui-datepicker { font-size: .7em; }
         .input-prompt { position: absolute; font-style: italic; color: #aaa; margin: 0.2em 0 0 0.5em; }
+        .headeroption { border: 2px solid black; margin: 0; padding: 0}
+        #initials {position: relative; left: 0px; right: 0px; top: 0px; width: 100%; }
+        .clicks { width: 100%; border: 0; padding: 0; margin: 0 }
+        .h { cursor:pointer }
+        .h:hover { background-color: #ccddee; }
 
 </style>
 </head>
 <body>
 <h2>Roller Derby Names</h2>
-<p id='dothis'>Loading...</p>
-<p><input id='derbyname' type='text' title='Start typing a deby name...' size=50 /></p>
-
- 
+<p><input id='derbyname' type='text' title='Start typing a derby name...' size=50 /></p>
+<p><div id='initials'>Loading...</div></p>
+<p id='content'>Content goes here</p>
 
 <script type='text/javascript'>
-  $(document).ready(function(){
+$(document).ready(function(){
    $('input[type=text][title],input[type=password][title],textarea[title]').each(function(i){
       $(this).addClass('input-prompt-' + i);
       var promptSpan = $('<span class="input-prompt"/>');
@@ -62,8 +66,21 @@
         if($(this).val() == ''){ $('#input-prompt-' + i).show(); }
       });
     });
-  });
-  $('#derbyname').autocomplete({ source: "http://rollerder.by/twoevils/ajax.php" });
+    $('#derbyname').autocomplete({ source: "http://rollerder.by/twoevils/ajax.php" });
+    $.ajax({ url: "ajax.php?action=list",
+        success: function(data) { 
+            var newhtml = "<table><tr>"
+            $("#initials").html("<table><tr>");
+            $.each(data, function(x,y) {
+                newhtml = newhtml + "<td class='h' data-value='"+y+"'>"+y+"</td>";
+            })
+            $("#initials").html(newhtml+"</tr></td></table>");
+            $(".h").click(function() {loadcontent($(this).data("value"))});
+        }
+    });
+});
 
-</script>
+function loadcontent(x) {
+    alert(x);
+}
 </script>
